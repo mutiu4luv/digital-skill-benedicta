9cba5bfd9772f83e39e8f8a73d580a051334f1a2
mutiu4luv <chidiemmamadu@gmail.com>
2025-11-07 14:17:07 +0100

    routes delete

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -18,7 +18,7 @@ router.post("/register", upload.single("profilePhoto"), registerUser);
 router.post("/verify-email", verifyEmail);
 router.post("/login", loginUser);
 router.get("/all", protect, authorizeRoles("owner"), getAllUsers);
-router.delete("/:id", protect, deleteUser);
-router.put("/:id", protect, updateUser);
+router.delete("/:id", protect, authorizeRoles("owner"), deleteUser);
+router.put("/:id", protect, authorizeRoles("owner"), updateUser);
 
 export default router;
