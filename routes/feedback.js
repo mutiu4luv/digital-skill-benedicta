a0c59588a0ed24292e8e69e8d89a6fbdcab58e31
mutiu4luv <chidiemmamadu@gmail.com>
2025-11-15 15:24:36 +0100

    feedback

--- routes/feedback.js
+++ routes/feedback.js
@@ -4,11 +4,16 @@ import {
   getCoachMonthlyRatings,
   submitFeedback,
 } from "../controller/feedback.js";
-import { authorizeRoles } from "../middleware/authMiddleware.js";
+import { authorizeRoles, protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 router.post("/", submitFeedback);
 router.get("/coaches-ratings", getCoachesRatings);
-router.get("/my-ratings", authorizeRoles("coach"), getCoachMonthlyRatings);
+router.get(
+  "/my-ratings",
+  protect,
+  authorizeRoles("coach"),
+  getCoachMonthlyRatings
+);
 
 export default router;
