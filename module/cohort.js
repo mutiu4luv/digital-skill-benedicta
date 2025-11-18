f9b5f3b7757ce3f9fe3900affd2c888c544b1861
mutiu4luv <chidiemmamadu@gmail.com>
2025-11-18 06:29:20 +0100

    create a cohort

--- module/cohort.js
+++ module/cohort.js
@@ -2,21 +2,27 @@ import mongoose from "mongoose";
 
 const cohortSchema = new mongoose.Schema(
   {
-    courseId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Course",
-      required: true,
-    },
-    coachId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
+    name: { type: String, required: true },
     ownerId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
     }, // the creator
+    courses: [
+      {
+        courseId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Course",
+          required: true,
+        },
+        coachId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+        durationInDays: { type: Number, required: true },
+      },
+    ],
     startDate: { type: Date },
     endDate: { type: Date },
     status: {
@@ -25,7 +31,6 @@ const cohortSchema = new mongoose.Schema(
       default: "not_started",
     },
     studentIds: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-    durationInDays: { type: Number, required: true },
   },
   { timestamps: true }
 );
